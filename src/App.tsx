d9ad7688494d393f8010cbedf4da981b6142bfa3
eve337 <vesunchik1945@gmail.com>
2022-10-16 17:52:49 +0300

    feat: add eslint, connect normalize, fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import RoutesStore from './routes/routes';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import RoutesStore from './routes/routes'
+import './styles/main.scss'
 
-function App() {
+function App (): JSX.Element {
   return (
     <div className="App">
       <Routes>
         {Object.values(RoutesStore).map((route) => (
-          <Route path={route.path} element={<route.component/>} />
+          <Route key={route.path} path={route.path} element={<route.component/>} />
         ))}
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
